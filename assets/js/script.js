285b0ee5a0d61873d6ec0ee1b1ee60f2a16b01a6
puentedg <puentedg@gmail.com>
2022-11-21 22:05:10 -0500

    history loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
-var APIKey = "59d0933cc9a4ce76bb2ab24bbae4e56c";
+var apiKey ="7cb8e0b712f8020b6129bd30e58c6bca";
 var mainWeather = document.querySelector ('.main-weather');
 var searchInputEl = document.querySelector('#search-input');
 var searchButton = document.querySelector('#search-btn');
 var forecastWeather = document.querySelector('.forecast')
 let historySelector = document.querySelector('#history-selector')
+
 // let historySelector = document.querySelector("#history-selector")
 let userHistory=document.querySelector('#search-history');
 let historyCache = [];
+var cityName = ""
+
+loadHistory()
 
 // function to get the input and calls the function to fetch results
 function getCity (event) {
@@ -15,7 +19,6 @@ function getCity (event) {
     console.log(city)
     getSearchResults(city)
     saveHistory(city);
-    // renderHistory();
 }
 //function gets the search from openweather api
 
@@ -27,22 +30,25 @@ function getSearchResults (city) {
         console.log(data);
         const lon = data.coord.lon
         const lat = data.coord.lat
-        displaySearchResults(data)
+        cityName = data.name
         getForecast (lon,lat)
+        displaySearchResults(data) 
     })
 }
 
 // function that gets the forecast from the api
 function getForecast(lon,lat){
+    var oneCall = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={minutely,hourly,alerts}&appid=${apiKey}&units=Imperial`
     var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=59d0933cc9a4ce76bb2ab24bbae4e56c&units=Imperial`
-    fetch(forecastURL)
+    fetch(oneCall)
     .then((res)=>{return res.json()})
     .then((data)=> {
         console.log(data);
-            
+        displaySearchResults(data)  
         displayForecastResults(data)
     })
 }
+
 // function display results of our search
 function displaySearchResults(data){
     mainWeather.innerHTML ="";
@@ -52,23 +58,25 @@ function displaySearchResults(data){
     cardHeader.setAttribute("class","card-header has-text-white has-text-weight-bold is-uppercase is-size-4")
     const cardTitle = document.createElement('h2')
     cardTitle.setAttribute ('class', "card-header-title has-text-weight-bold is-align-content-center")
-    cardTitle.textContent = data.name
+    cardTitle.textContent = cityName
     const icon = document.createElement('img')
-    icon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png")
+    icon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png")
     const span = document.createElement('span')
     const cardBody = document.createElement('div')
     cardBody.setAttribute("class", "card-content")
     const temp = document.createElement('p')
     const humidity = document.createElement('p')
     const wind = document.createElement('p')
-    temp.textContent = `Temperature: ${data.main.temp} F`
-    humidity.textContent = `Humidity: ${data.main.humidity} %`
-    wind.textContent = `Wind speed: ${data.wind.speed} MPH`
+    const uvi= document.createElement('p')
+    temp.textContent = `Temperature: ${data.current.temp} F`
+    humidity.textContent = `Humidity: ${data.current.humidity} %`
+    wind.textContent = `Wind speed: ${data.current.wind_speed} MPH`
+    uvi.textContent = `UVI: ${data.current.uvi}`
 
     span.append(icon)
     cardTitle.append(span)
     cardHeader.append(cardTitle)
-    cardBody.append(temp,humidity,wind)
+    cardBody.append(temp,humidity,wind,uvi)
     card.append(cardHeader,cardBody)
     mainWeather.append(card)
 
@@ -92,7 +100,7 @@ function displayForecastResults(data){
         cardTitle.setAttribute ('class', "card-header-title title is-4 has-text-weight-bold is-align-content-center")
         cardTitle.textContent = data.name
         const icon = document.createElement('img')
-        icon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[index].weather[0].icon + ".png")
+        icon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.daily[index].weather[0].icon + ".png")
         icon.setAttribute("class", "weather-icon")
         const span = document.createElement('span')
         const cardBody = document.createElement('div')
@@ -100,9 +108,9 @@ function displayForecastResults(data){
         const temp = document.createElement('p')
         const humidity = document.createElement('p')
         const wind = document.createElement('p')
-        temp.textContent = `Temperature: ${data.list[index].main.temp} F`
-        humidity.textContent = `Humidity: ${data.list[index].main.humidity} %`
-        wind.textContent = `Wind speed: ${data.list[index].wind.speed} MPH`
+        temp.textContent = `Temperature: ${data.daily[index].temp.day} F`
+        humidity.textContent = `Humidity: ${data.daily[index].humidity} %`
+        wind.textContent = `Wind speed: ${data.daily[index].wind_speed} MPH`
         
         span.append(icon)
         cardTitle.append(span, date)
@@ -113,11 +121,9 @@ function displayForecastResults(data){
        }
 }
 
-function renderHistory() {
+function renderHistory(history) {
     // if any history objects are disabled, do not delete them
-   
-    console.log(historyCache);
-    historyCache.forEach (element => {
+    history.forEach (element => {
         let userHistoryItem = document.createElement("li");
         const btn = document.createElement("button");
         btn.textContent = element;
@@ -126,7 +132,6 @@ function renderHistory() {
         userHistoryItem.append(btn)
         historySelector.append(userHistoryItem);
     })
-    // created a variable for search history and added an area to append the history
 }
 
 // This function loads the history from localstorage from the localStorageKey and parses it from json
@@ -139,21 +144,21 @@ function loadHistory() {
 
     console.log(historyCache)
 
-    renderHistory()
+    renderHistory(history)
     
 }
 
 // if the search history has the same term already, it is moved to the start of the list
 function saveHistory(city) {
 
-    if (historyCache.includes(city)){
+    if (!historyCache.includes(city)){
         historyCache.push(city)
         localStorage.setItem("searchResult", JSON.stringify(historyCache));
         console.log(historyCache);
         console.log(historyCache.length);
     }
 
-    // check if city is null or "", if it is etiher, return;
+    // check if city is null or "", if it is either, return;
     if (city == null || city == "") {
         console.log ("variable is null or undefined");
         return;
@@ -162,6 +167,7 @@ function saveHistory(city) {
 
 loadHistory();
 }
+
 // Event Listener for check the weather button
 searchButton.addEventListener("click", getCity);
 
@@ -169,7 +175,7 @@ searchButton.addEventListener("click", getCity);
 function historyButtonClicked(event) {
     event.preventDefault();
     
-    var userHistoryEl = $(event.target).text();
+    var userHistoryEl = event.target.textContent;
     
     getSearchResults(userHistoryEl);
 }
